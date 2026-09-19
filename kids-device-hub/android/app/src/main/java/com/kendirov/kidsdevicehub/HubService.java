613782b99bb47a1970017fa60d922b9e9b9d88fc
kendirov <93733593+kendirov@users.noreply.github.com>
2026-09-19 11:32:03 +0300

    Kids Device Hub: fix HubService.java

--- kids-device-hub/android/app/src/main/java/com/kendirov/kidsdevicehub/HubService.java
+++ kids-device-hub/android/app/src/main/java/com/kendirov/kidsdevicehub/HubService.java
@@ -5,7 +5,6 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import androidx.annotation.Nullable;
 
 public class HubService extends Service {
     private HubApiServer server;
@@ -26,5 +25,5 @@ public class HubService extends Service {
     }
 
     @Override public void onDestroy() { if(server!=null) server.close(); super.onDestroy(); }
-    @Nullable @Override public IBinder onBind(Intent intent) { return null; }
+    @Override public IBinder onBind(Intent intent) { return null; }
 }
